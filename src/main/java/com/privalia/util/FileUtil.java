a5f612761e62fc936c05a4480612ed3ca49fc2d1
Jaume Singla <jaume.singla@privalia.com>
2018-01-23 16:58:52 +0100

    adding logger in the FileUtil and some things!

--- src/main/java/com/privalia/util/FileUtil.java
+++ src/main/java/com/privalia/util/FileUtil.java
@@ -1,21 +1,27 @@
 package com.privalia.util;
 
+import org.apache.log4j.Logger;
+
 import java.io.File;
 import java.io.IOException;
 
+
 public class FileUtil {
-    public static synchronized boolean createFile(String file) {
+
+    static final Logger logger = Logger.getLogger(FileUtil.class);
+
+    public static synchronized boolean createFile(String file) throws IOException {
+        boolean isFileCreated=false;
         File f = new File(file);
         if (!f.exists()) {
             try {
-                f.createNewFile();
+                isFileCreated = f.createNewFile();
             } catch (IOException e) {
-                e.printStackTrace();
+                logger.error(e.getMessage());
+                throw e;
             }
-            return true;
-        } else {
-            return false;
         }
+        return isFileCreated;
 
     }
 
