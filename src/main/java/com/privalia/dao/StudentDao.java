b322fa4301b40d21e7d896f4adc85c22155b6324
Jaume Singla <jaume.singla@privalia.com>
2018-01-23 17:47:11 +0100

    Adding loggers

--- src/main/java/com/privalia/dao/StudentDao.java
+++ src/main/java/com/privalia/dao/StudentDao.java
@@ -3,10 +3,12 @@ package com.privalia.dao;
 import com.privalia.model.Student;
 import com.privalia.util.Config;
 import com.privalia.util.FileUtil;
+import org.apache.log4j.Logger;
 
 import java.io.*;
 
 public class StudentDao implements IDao<Student>{
+    private static final Logger logger = Logger.getLogger(StudentDao.class);
     @Override
     public int add(Student student) throws IOException {
         String fileName = Config.getInstance().getFilename();
@@ -17,6 +19,9 @@ public class StudentDao implements IDao<Student>{
         try (BufferedWriter bfw = new BufferedWriter(osw)) {
             bfw.write(student.toString());
             bfw.newLine();
+        } catch (IOException e){
+            logger.error(e.getMessage());
+            throw e;
         }
         return 0;
     }
