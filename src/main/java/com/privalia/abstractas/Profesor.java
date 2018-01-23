c9c25bc078c6fef6bb162a06fae0792b7b50350d
Jaume Singla <jaume.singla@privalia.com>
2018-01-23 15:06:03 +0100

    Abstract class

---
+++ src/main/java/com/privalia/abstractas/Profesor.java
@@ -0,0 +1,9 @@
+package com.privalia.abstractas;
+
+public abstract class Profesor {
+    abstract public float importeNomina();
+
+    public void prueba(){
+        System.out.println("Metodo no abstracto");
+    }
+}
---
+++ src/main/java/com/privalia/abstractas/ProfesorInterino.java
@@ -0,0 +1,9 @@
+package com.privalia.abstractas;
+
+public class ProfesorInterino extends Profesor {
+    @Override
+    public float importeNomina() {
+        super.prueba();
+        return 0;
+    }
+}
