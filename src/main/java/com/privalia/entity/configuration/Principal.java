f68d49935d41c063d4370d411bc8bee3ada2b269
Jaume Singla <jaume.singla@privalia.com>
2018-02-08 14:51:54 +0100

    Changed the configuration to load the properties

--- src/main/java/com/privalia/entity/configuration/Principal.java
+++ src/main/java/com/privalia/entity/configuration/Principal.java
@@ -5,12 +5,8 @@ import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Principal {
-    public static void main(String[] args){
 
-        @Cleanup AbstractApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
-
-        Student student = (Student) context.getBean("student");
-
+    static String getString(Student student) {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append(student.getIdStudent());
         stringBuilder.append(" ");
@@ -23,8 +19,20 @@ public class Principal {
         stringBuilder.append(student.getAddress().getIdAddress());
         stringBuilder.append(" ");
         stringBuilder.append(student.getAddress().getStreet());
+        return stringBuilder.toString();
+    }
+    public static void main(String[] args){
 
-        System.out.println(stringBuilder.toString());
+        @Cleanup AbstractApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
+
+        Student student = (Student) context.getBean("student");
+
+
+        System.out.println(getString(student));
+
+        Student studentConstructor = (Student) context.getBean("studentConstructor");
+
+        System.out.println(getString(studentConstructor));
 
     }
 }
