146047a11761e477eff4fcc61be81847296331e3
Jaume Singla <jaume.singla@privalia.com>
2018-02-14 17:51:40 +0100

    Adding testing for profiles

--- src/main/java/com/privalia/perfiles/User.java
+++ src/main/java/com/privalia/perfiles/User.java
@@ -1,8 +1,10 @@
 package com.privalia.perfiles;
 
+import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
 @ToString
+@EqualsAndHashCode
 public class User {
     public Integer id;
     public String name;
