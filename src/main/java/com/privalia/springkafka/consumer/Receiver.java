c02229911e04faf04c682d21606857c6982077a2
Jaume Singla <jaume.singla@privalia.com>
2018-03-07 17:06:53 +0100

    Adding the receiver and the status

---
+++ src/main/java/com/privalia/springkafka/consumer/Receiver.java
@@ -0,0 +1,27 @@
+package com.privalia.springkafka.consumer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+
+import java.util.ArrayList;
+import java.util.concurrent.CountDownLatch;
+
+@Slf4j
+public class Receiver {
+
+    public static ArrayList<String> receiveMessagesList = new ArrayList<>();
+
+    private CountDownLatch latch = new CountDownLatch(1);
+
+    public CountDownLatch getLatch(){
+        return latch;
+    }
+
+    @KafkaListener(topics="${kafka.topic.helloworld}")
+    public void receive(String payload){
+        log.info("Received payload='{}", payload);
+        receiveMessagesList.add(payload);
+        latch.countDown();
+    }
+
+}
---
+++ src/main/java/com/privalia/springkafka/controllers/SpringKafkaController.java
@@ -0,0 +1,31 @@
+package com.privalia.springkafka.controllers;
+
+import com.privalia.springkafka.consumer.Receiver;
+import com.privalia.springkafka.producer.Sender;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/kafka")
+public class SpringKafkaController {
+
+    @Value("${kafka.topic.helloworld}")
+    private String topic;
+
+    @Autowired
+    private Sender sender;
+
+    @RequestMapping(value = "/messages/", method = RequestMethod.GET, produces = "application/json")
+    public List<String> getMessages(){
+        return Receiver.receiveMessagesList;
+    }
+
+    @RequestMapping(value="/messages/", method= RequestMethod.POST, produces="test/plain")
+    public String createMessage(@RequestBody String message){
+        sender.send(topic, message);
+        return "Message: "+message;
+    }
+}
